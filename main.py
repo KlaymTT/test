79e0dd13020abd325c0d2fb03dd131a08c16fbd3
KlaymTT <vlad-sviridov-02@mail.ru>
2021-10-06 03:44:49 +0300

    calc

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ b = float(input("Введите второе число: "))
 c = input("Знак: ")
 if c == "+":
     print("Результат:",a+b)
+elif c == "/" and b ==0:
+    print("Результат: 0")
 elif c == "-":
     print("Результат:",a-b)
 elif c == "/":
